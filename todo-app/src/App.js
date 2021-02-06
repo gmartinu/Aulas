5b928103b99941f869901965da4bee060e844e95
Gabriel Martinusso <gabrielbramos@outlook.com>
2021-02-06 01:31:17 -0300

    Change folder name

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,30 +1,12 @@
 // import './App.css';
-import React, { useState } from 'react';
-import Buttons from './buttons';
-import { Typography } from '@material-ui/core';
+import React from 'react';
 
 const App = () => {
-  const [value, setValue] = useState(0);
-
-  const increment = () => {
-    setValue(value + 1)
-  }
-
-  const decrement = () => {
-    setValue(value - 1)
-  }
-
   return (
     <>
-      <Typography variant="h3">{value}</Typography>
-      <Buttons increment={increment} decrement={decrement}/>
+
     </>
   );
 }
 
 export default App;
-
-//haha marcos gay
-//positivo capitao hahaha borges
-//Haha ficar milhonario com programinha
-//traveco 2 real
